6dd160d84e526db375ddb8670bbad0a3f3e06380
UGEcko <vanhelsing314@outlook.com>
2024-06-17 18:44:59 -0400

    FontPlus update
    Revisions:
    Rewrote most of the FontPlus structure
    Supports both pre-made fonts and custom fonts.
    Doesn't interact with the RMCache anymore.

--- mod.ts
+++ mod.ts
@@ -1,72 +1,113 @@
-import { existsSync  } from "https://deno.land/std/fs/mod.ts";
+import {existsSync } from "https://deno.land/std/fs/mod.ts";
 import * as path from 'https://deno.land/std/path/mod.ts';
+import {rand} from "https://deno.land/x/remapper@3.1.2/src/mod.ts";
 
-//! Attention: This module requires read-write permissions via deno, use of getFont() requires initFonts() before any declaration of getFont().
+function h(l:number) {
+    const r = 'sadojkoop2392038i90id90mocmsdklkljdklasue9o'
+    let s = 'font_'
+    for(let i = 0; i < l; i++) {
+        s += r[rand(0,r.length,1)]
+    }
+    return s
+}
+let version: rmVersion;
 
-import {cacheData, rand} from "https://deno.land/x/remapper@3.1.2/src/mod.ts";
+
+const fontDir = 'RMFonts'
+
 const Fonts = {
-    lite: "https://raw.githubusercontent.com/UGEcko/FontPlus/main/Fonts/lite.rmmodel",
-    swifter: "https://raw.githubusercontent.com/UGEcko/FontPlus/main/Fonts/swifter.rmmodel",
-    tzur: "https://raw.githubusercontent.com/UGEcko/FontPlus/main/Fonts/tzur.rmmodel",
-    gameplay: "https://raw.githubusercontent.com/UGEcko/FontPlus/main/Fonts/v3/GameplayFont.rmmodel"
-};
-let cachedFonts: (keyof typeof Fonts)[] = [];
+    Lite: "lite",
+    Swifter: "swifter",
+    Tzur: "tzur",
+    Gameplay: "gameplay"
+} // Premade fonts
 
-export function initFonts() { // This is so the RM_Cache doesnt limit access / delete the font data if no fonts are being used.
-    cacheData("Fonts", () => {
-        const data = [""] // Dummy 
-        return data;
-    },[rand(0,10,1)]);
-    cachedFonts = []
+type rmVersion = "v3" //| "v4"
+
+type yapLevel = "Mild" | "Medium" | "Youreadumbass"
+function log(text: string, whatareweyappingabout: yapLevel) {
+    switch(whatareweyappingabout){
+        case "Mild":
+            console.log(`%c[FontPlus] : ${text}`, "color:green")
+            break;
+        case "Medium":
+            console.log(`%c[FontPlus] : ${text}`, "color:orange")
+            break;
+        case "Youreadumbass":
+            console.log(`%c[FontPlus] : ${text}`, "color:red")
+            break;
+    }
+    
 }
 
-function cache_font(font: (keyof typeof Fonts)[]) {
-    if(cachedFonts.includes(font[0])) { // If its already in the cache, dont do anything.
-        return;
-    } else {
-        if(!cachedFonts.includes(font[0])) {
-            cachedFonts.push(font[0]);
-        };
+export function initFonts(RemapperVersion: rmVersion) {
+    if(RemapperVersion == "v3") {
+        version = "v3"
+    } /* else { // RM4
 
-        cacheData("Fonts", () => { // push the cache array to fonts cache
-            const data = cachedFonts
-            return data;
-        },[rand(0,10,1)]);
+    } */
+}
+
+async function getFontData(URL: string, custom?:boolean) {
+    try {
+        if(!custom) {
+            const id = `https://raw.githubusercontent.com/UGEcko/FontPlus/main/Fonts/${version}/${URL}.rmmodel`
+            const web = await fetch(id);
+            const content = await web.text();
+            return content as string;
+        } else {
+            const web = await fetch(URL);
+            const content = await web.text();
+            return content as string;
+        }
+    } catch(err) {
+        log(err,"Medium")
     }
 }
 
-async function downloadFont(URL: string, fontPath: string) {
+function ensureDirectory(data?: string, identifier?: string) {
     try {
-        const web = await fetch(URL)
-        const content = await web.text();
-        await Deno.writeTextFile(fontPath,content);
-       
-    } catch(ex) {
-        console.log("DOWNLOAD FONT ERROR " + ex);
-    }
-}
-
-async function manageFontDirectory(fonts: (keyof typeof Fonts)[]) {
-    try {
-        const customDirPath = path.join(path.dirname(path.fromFileUrl(Deno.mainModule)), 'RemapperFonts');
-        await Deno.mkdir(customDirPath, { recursive: true });
-
-        const downloadPromises = fonts.map(async (font) => {
-            const fontDir = `${customDirPath}\\${font}.rmmodel`;
-            if (!existsSync(fontDir)) {
-                await downloadFont(Fonts[font], fontDir);
+        const dirPath = path.join(path.dirname(path.fromFileUrl(Deno.mainModule)),fontDir) // ./RMFonts
+        if(!existsSync(dirPath, {isDirectory : true})) { // Check if the RMFonts folder doesnt exist to make it.
+            Deno.mkdir(dirPath, {recursive : true})
+        }
+        if(data && identifier) { // Will download the font if data & identifier is filled.
+            let path = ''
+            if(!identifier.includes('.rmmodel')) {
+                path = `${dirPath}\\${identifier}.rmmodel` 
+            } else {
+                path = `${dirPath}\\${identifier}`
             }
-        });
-
-        await Promise.all(downloadPromises); // Wait for all fonts to download
-    } catch (error) {
-        console.error("Error creating tempdir:", error);
+            if(!existsSync(path)) { // Write if the font doesnt exist.
+                Deno.writeTextFileSync(path, data)
+            }
+            return path;
+        }
+    } catch(err) {
+        log(err,"Medium")
     }
 }
 
-export async function getFont(font: (keyof typeof Fonts)) {
-        cache_font([font]);
-        await manageFontDirectory([font]); // Download/handle font before returning the path
-        const fontPath = `./RemapperFonts\\${font}.rmmodel`;
-    return fontPath;
+export async function getFont(font: (keyof typeof Fonts) | undefined, customFont?: string): Promise<string> {
+    if(version) {
+            let path = '';
+            if(font != undefined) { // If you're using a pre-made font.
+                const selectFont = Fonts[font];
+                const pathx = ensureDirectory(await getFontData(selectFont,false), font) as string
+                path = pathx
+            }
+            else if(customFont) { // If you're using a custom font.
+                const identifer = customFont.slice(customFont.lastIndexOf('/')+1);
+                const pathx = ensureDirectory(await getFontData(customFont,true), identifer) as string
+                path = pathx
+            }
+            else {
+                log("getFont() Is not filled in. Please use a pre made or custom font.","Youreadumbass")
+            }
+            return path as string;
+    } else {
+        log("Please initiate FontPlus by using the init() function.", "Medium")
+        return '';
+    }
 }
+
